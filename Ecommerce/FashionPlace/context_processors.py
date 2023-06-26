93b44a176bbfb74003652961ad5713e65796a65a
Dolapo Adegoju <tichiegoju@gmail.com>
2023-06-26 02:40:54 +0100

    Fixed anonymous user feature

--- Ecommerce/FashionPlace/context_processors.py
+++ Ecommerce/FashionPlace/context_processors.py
@@ -7,16 +7,14 @@ def category_links(request):
 def cart_renderer(request):
     try:
         if request.user.is_authenticated:
-            client = request.user.client
-            cart, created = Cart.objects.get_or_create(client=client, completed=False)
+            cart = Cart.objects.get(client=request.user.client, completed=False)
             cartitems = cart.cartitem_set.all()
         else:
-            cart = Cart.objects.get(session_id=request.session['nonuser'], completed=False)
-            cartitems = []
-            
-    except:
-        cart = []
+            session_id = request.session.get('session_id')
+            cart = Cart.objects.get(session_id=session_id, completed=False)
+            cartitems = cart.cartitem_set.all()
+    except Cart.DoesNotExist:
         cartitems = []
-        cart = {'cartquantity': 0}
+        cart = {"get_cart_total": 0, "get_total": 0}
         
     return {'cart': cart, 'cartitems': cartitems}
--- Ecommerce/FashionPlace/views.py
+++ Ecommerce/FashionPlace/views.py
@@ -1,26 +1,13 @@
 from django.shortcuts import render
-from .models import *
+from django.contrib.sessions.models import Session
 from django.http import JsonResponse
+from .models import *
 import json
+import uuid
 
 
 # Create your views here.
-def get_cart_and_items(request):
-    if request.user.is_authenticated:
-        client = request.user.client
-        cart, created = Cart.objects.get_or_create(client=client, completed=False)
-        cartitems = cart.cartitem_set.all()
-    else:
-        cart = []
-        cartitems = []
-        cart = {'cartquantity': 0}
-
-    return cart, cartitems
-
-
 def home(request):
-    cart, cartitems = get_cart_and_items(request)
-
     new_arrivals = Product.objects.filter(new_arrivals=True)
     top_rated = Product.objects.filter(top_rated=True)
     trending = Product.objects.filter(trending=True)
@@ -28,58 +15,36 @@ def home(request):
     return render(request, 'home.html', {
         'new_arrivals': new_arrivals,
         'top_rated': top_rated,
-        'trending': trending,
-        'cart': cart
+        'trending': trending
     })
 
 
 def all_products(request):
-    cart, cartitems = get_cart_and_items(request)
-
     products = Product.objects.all()
-
     return render(request, 'products.html', {
-        'products': products,
-        'cart': cart
+        'products': products
     })
 
 
 def all_category(request, slug):
-    cart, cartitems = get_cart_and_items(request)
-
     category = Category.objects.get(slug=slug)
     products = Product.objects.filter(category=category)
-
     return render(request, 'category.html', {
         'category': category,
-        'products': products,
-        'cart': cart
+        'products': products
     })
 
 
 def search_product(request):
-    cart, cartitems = get_cart_and_items(request)
-
     product_name = request.POST.get('product_name')
     searched_products = Product.objects.filter(name__icontains=product_name)
-
     return render(request, 'search.html', {
         'searched_products': searched_products,
-        'cart': cart
     })
 
 
 def cart(request):
-    if request.user.is_authenticated:
-        client = request.user.client
-        cart, created = Cart.objects.get_or_create(client=client, completed=False)
-        cartitems = cart.cartitem_set.all()
-    else:
-        cartitems = []
-        cart = {"get_cart_total": 0, "get_total": 0}
-
-    return render(request, 'cart.html', {'cartitems': cartitems,
-    'cart': cart})
+    return render(request, 'cart.html')
 
 def updatecart(request):
     data = json.loads(request.body)
@@ -100,16 +65,34 @@ def updatecart(request):
         msg = {
             'quantity': cart.get_cart_item
         }
+    else:
+        session_id = request.session.get('session_id')
+        product = Product.objects.get(product_id=product_id)
+        cart,created = Cart.objects.get_or_create(session_id=session_id, completed=False)
+        cartitems, created =CartItem.objects.get_or_create(cart=cart, product=product)
+        if action == 'add':
+            cartitems.quantity += 1
+        cartitems.save()
 
+        msg = {
+            'quantity': cart.get_cart_item
+        }
+        
     return JsonResponse(msg, safe=False)
+
 
 def updatequantity(request):
     data = json.loads(request.body)
     inputval = int(data['in_val'])
     product_id = data['p_id']
+    
+    if inputval < 0:
+        # Set inputval to 0 if it's less than zero
+        inputval = 0
+
     if request.user.is_authenticated:
         client = request.user.client
-        product = Product.objects.get(product_id= product_id)
+        product = Product.objects.get(product_id=product_id)
         cart, created = Cart.objects.get_or_create(client=client, completed=False)
         cartitems, created = CartItem.objects.get_or_create(product=product, cart=cart)
 
@@ -117,7 +100,21 @@ def updatequantity(request):
         cartitems.save()
 
         msg = {
-            'subtotal':cartitems.get_total,
+            'subtotal': cartitems.get_total,
+            'grandtotal': cart.get_cart_total,
+            'quantity': cart.get_cart_item
+        }
+    else:
+        session_id = request.session.get('session_id')
+        product = Product.objects.get(product_id=product_id)
+        cart, created = Cart.objects.get_or_create(session_id=session_id, completed=False)
+        cartitems, created = CartItem.objects.get_or_create(cart=cart, product=product)
+
+        cartitems.quantity = inputval
+        cartitems.save()
+
+        msg = {
+            'subtotal': cartitems.get_total,
             'grandtotal': cart.get_cart_total,
             'quantity': cart.get_cart_item
         }
@@ -127,8 +124,7 @@ def updatequantity(request):
 
 
 def checkout(request):
-    cart, cartitems = get_cart_and_items(request)
-    return render(request, 'checkout.html', {'cart': cart, 'cartitems': cartitems})
+    return render(request, 'checkout.html')
 
 def payment(request):
     data = json.loads(request.body)
