9d0e289e9aa3fd29f706fe532557a61c71faaf39
Dolapo Adegoju <tichiegoju@gmail.com>
2023-07-11 04:22:43 +0100

    Thank you Jesus!!!!

--- FashionPlace/context_processors.py
+++ FashionPlace/context_processors.py
@@ -15,6 +15,7 @@ def cart_render(request):
             try:
                 cart = Cart.objects.get(session_id = request.session['nonuser'], completed=False)
             except:
+                request.session['nonuser'] = str(uuid.uuid4())
                 cart = Cart.objects.create(session_id = request.session['nonuser'], completed=False)
     cartitems = cart.cartitem_set.all()
     return {'cart': cart, 
